e046007bd06802ec140c6877fdbd64c4d161cf9c
Alan Moraes Souza <alan@souza.eti.br>
2023-06-14 19:22:34 -0300

    Versão com ts mais novo.

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
+import { helloTeste } from "./teste.js";
+
 const world = 'world';
 
 export function hello(who: string = world): void {
     console.log(`Hello ${who}! `);
+    helloTeste(who);
 }
---
+++ src/teste.ts
@@ -0,0 +1,5 @@
+const worldTeste = 'worldTeste';
+
+export function helloTeste(who: string = worldTeste): void {
+    console.log(`HelloTeste ${who}! `);
+}
