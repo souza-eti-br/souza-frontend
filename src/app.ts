aff6c4aab10422402cf388e1ebe5b112771aa52f
Alan Moraes Souza <alan@souza.eti.br>
2024-02-12 20:11:13 -0300

    Estrutura i18n

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,16 @@
 import {Menu} from "parts/menu";
+import {I18n} from "utils/i18n";
 
 (new Menu()).print();
+let i18n = new I18n();
+i18n.addListener((i18n: I18n) : void => {
+    console.log("teste");
+});
+i18n.addListener((i18n: I18n) : void => {
+    console.log(i18n.getMessage("welcome"));
+    console.log(i18n.getMessage("welcome-text"));
+    console.log(i18n.getMessage("footer-label"));
+    console.log(i18n.getMessage("new"));
+});
+i18n.setLanguage("es");
+i18n.setLanguage("pt");
