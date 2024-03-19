0dd8ac2bf657ce61faaa4b589e099cafb079fdf7
Alan Moraes Souza <alan@souza.eti.br>
2024-03-19 12:27:31 -0300

    Incluindo tela de carregando

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
+import { LoadingComponent } from './loading/loading.component';
 
 @NgModule({
-    declarations: [ AppComponent ],
+    declarations: [ AppComponent, LoadingComponent ],
     imports: [ BrowserModule, AppRoutingModule ],
     providers: [],
     bootstrap: [AppComponent]
