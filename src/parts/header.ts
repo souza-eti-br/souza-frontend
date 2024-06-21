2543a31be251a3e1bede0022d27b5217d3246379
Alan Moraes Souza <alan@souza.eti.br>
2024-06-21 16:17:37 -0300

    Language selector transformado em componente.

--- src/parts/header.ts
+++ src/parts/header.ts
@@ -1,20 +1,16 @@
 import html from "../utils/html";
 import i18n from "services/i18n";
 import HeaderLogo from "components/header-logo";
+import LanguageSelector from "components/language-selector";
 
 export default class Header {
     main: HTMLDivElement;
-    select: HTMLSelectElement;
+    select: LanguageSelector;
     constructor() {
         this.main = html.div("header");
         this.main.id = "header";
+        this.select = new LanguageSelector();
         this.main.appendChild(new HeaderLogo());
-        this.select = html.select("i18n-selector", (value: string) => {
-            i18n.setLanguage(value);
-        });
-        this.select.add(html.option("English", "en"));
-        this.select.add(html.option("Español", "es"));
-        this.select.add(html.option("Português", "pt"));
         this.main.appendChild(this.select);
         document.body.appendChild(this.main);
     }
