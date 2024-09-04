620d3bd14dfe150de36c9b1f7403a19a50bf5364
Alan Moraes Souza <alan@souza.eti.br>
2024-09-04 14:49:15 -0300

    Correção na route

--- src/parts/header/header.component.ts
+++ src/parts/header/header.component.ts
@@ -1,11 +1,12 @@
 import { Component } from "@angular/core";
+import { RouterLink } from '@angular/router';
 import { I18nService } from "../../services/i18n.service";
 import { LanguageSelectorComponent } from "../../components/language-selector/language-selector.component";
 
 @Component({
     selector: "ams-header",
     standalone: true,
-    imports: [ LanguageSelectorComponent ],
+    imports: [ RouterLink, LanguageSelectorComponent ],
     templateUrl: "./header.component.html",
     styleUrl: "./header.component.css"
 })
