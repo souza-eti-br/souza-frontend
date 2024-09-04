4bbcc870f626f2984a8a527483ca2ce20fb5000f
Alan Moraes Souza <alan.souza@cronapp.io>
2024-09-04 01:58:08 -0300

    Versão inicial!

--- src/parts/header/header.component.ts
+++ src/parts/header/header.component.ts
@@ -1,6 +1,7 @@
 import { Component } from "@angular/core";
 import { MenubarModule } from "primeng/menubar";
 import { MenuItem } from "primeng/api";
+import { I18nService } from "../../services/i18n.service";
 import { LanguageSelectorComponent } from "../../components/language-selector/language-selector.component";
 
 @Component({
@@ -11,132 +12,10 @@ import { LanguageSelectorComponent } from "../../components/language-selector/la
     styleUrl: "./header.component.css"
 })
 export class HeaderComponent {
-    items = [
-            {
-                label:'File',
-                icon:'pi pi-fw pi-file',
-                items:[
-                    {
-                        label:'New',
-                        icon:'pi pi-fw pi-plus',
-                        items:[
-                        {
-                            label:'Bookmark',
-                            icon:'pi pi-fw pi-bookmark'
-                        },
-                        {
-                            label:'Video',
-                            icon:'pi pi-fw pi-video'
-                        },
-
-                        ]
-                    },
-                    {
-                        label:'Delete',
-                        icon:'pi pi-fw pi-trash'
-                    },
-                    {
-                        separator:true
-                    },
-                    {
-                        label:'Export',
-                        icon:'pi pi-fw pi-external-link'
-                    }
-                ]
-            },
-            {
-                label:'Edit',
-                icon:'pi pi-fw pi-pencil',
-                items:[
-                    {
-                        label:'Left',
-                        icon:'pi pi-fw pi-align-left'
-                    },
-                    {
-                        label:'Right',
-                        icon:'pi pi-fw pi-align-right'
-                    },
-                    {
-                        label:'Center',
-                        icon:'pi pi-fw pi-align-center'
-                    },
-                    {
-                        label:'Justify',
-                        icon:'pi pi-fw pi-align-justify'
-                    },
-
-                ]
-            },
-            {
-                label:'Users',
-                icon:'pi pi-fw pi-user',
-                items:[
-                    {
-                        label:'New',
-                        icon:'pi pi-fw pi-user-plus',
-
-                    },
-                    {
-                        label:'Delete',
-                        icon:'pi pi-fw pi-user-minus',
-
-                    },
-                    {
-                        label:'Search',
-                        icon:'pi pi-fw pi-users',
-                        items:[
-                        {
-                            label:'Filter',
-                            icon:'pi pi-fw pi-filter',
-                            items:[
-                                {
-                                    label:'Print',
-                                    icon:'pi pi-fw pi-print'
-                                }
-                            ]
-                        },
-                        {
-                            icon:'pi pi-fw pi-bars',
-                            label:'List'
-                        }
-                        ]
-                    }
-                ]
-            },
-            {
-                label:'Events',
-                icon:'pi pi-fw pi-calendar',
-                items:[
-                    {
-                        label:'Edit',
-                        icon:'pi pi-fw pi-pencil',
-                        items:[
-                        {
-                            label:'Save',
-                            icon:'pi pi-fw pi-calendar-plus'
-                        },
-                        {
-                            label:'Delete',
-                            icon:'pi pi-fw pi-calendar-minus'
-                        },
-
-                        ]
-                    },
-                    {
-                        label:'Archieve',
-                        icon:'pi pi-fw pi-calendar-times',
-                        items:[
-                        {
-                            label:'Remove',
-                            icon:'pi pi-fw pi-calendar-minus'
-                        }
-                        ]
-                    }
-                ]
-            },
-            {
-                label:'Quit',
-                icon:'pi pi-fw pi-power-off'
-            }
-        ];
+    items = [{
+        label: this.i18n.get("home"),
+        icon: "pi pi-fw pi-home",
+        routerLink: "homee"
+    }];
+    constructor(public i18n: I18nService) {}
 }
